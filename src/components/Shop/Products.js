d6a9a4eae1ceaab60cc53508a6eb9508ab65bc87
Luis Peraza <leperazas@gmail.com>
2025-05-08 14:50:43 +0200

    Se agrega la funcionalidad de add to car, se corrige funcion de addItem en cart-slice

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,19 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
+import { DUMMY_PRODUCTS } from '../../DUMMY_DATA/DUMMY_PRODUCTS';
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => <ProductItem
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          price={item.price}
+          description={item.description}
+        />)}
       </ul>
     </section>
   );
--- src/components/store/cart-slice.js
+++ src/components/store/cart-slice.js
@@ -11,7 +11,16 @@ name: 'cart',
 initialState: initialCartState,
 reducers: {
     addItem: (state, action) => {
-        state.items = state.items.shift(action.payload.item);
+        const itemSended = action.payload.item;
+
+        const indexItem = state.items.findIndex((item) => item.id === itemSended.id );
+
+        if (indexItem >= 0) {
+            state.items[indexItem].quantity += 1;
+        } else {
+            state.items.unshift({...action.payload.item, quantity: 1});
+        }
+
     },
     removeItem: (state, action) => {
         state.items = state.items.filter((item) => item.id !== action.payload.id);
